fba89250fd5a5abe7102a168a6813598da34acd6
Richard Osborne <richard@xmos.com>
2013-11-23 20:19:51 +0000

    Flesh out map_generator executable.

--- main.cpp
+++ main.cpp
@@ -1,53 +1,107 @@
 #include "map_generator.h"
 #include <iostream>
+#include <fstream>
 #include <random>
+#include <cstdlib>
+#include <cctype>
+#include <regex>
+#include <numeric>
 
-static map_generator::IntMap testmap()
+using map_generator::IntMap;
+using map_generator::Steps;
+using map_generator::CodeCost;
+
+IntMap read_map(std::istream &in)
 {
-  map_generator::IntMap map;
-  std::default_random_engine generator;
-  std::uniform_int_distribution<int> distribution;
-  auto rand = std::bind(distribution, generator);
-  for (unsigned i = 0; i < 1000; i++) {
-    map.insert(std::make_pair(rand(), rand()));
+  IntMap map;
+  std::string contents;
+  while (1) {
+    auto c = in.get();
+    if (!in.good())
+      break;
+    contents.push_back(c);
   }
-  return map;
-}
-
-static map_generator::IntMap testmap2()
-{
-  map_generator::IntMap map;
-  for (unsigned i = 0; i < 256; i++) {
-    unsigned unzipped = 0;
-    for (unsigned j = 0; j < 8; j++) {
-      if (i & (1 << j)) {
-        unzipped |= 1 << (j * 4);
+  std::regex regex(R"(^(,?)(\d+)\s*,(\d+)\s*)");
+  auto it =
+  std::sregex_iterator(contents.begin(), contents.end(), regex);
+  auto end = std::sregex_iterator();
+  bool first = true;
+  for (; it != end;) {
+    auto match = *it;
+    bool leading_comma = match[1].length() != 0;
+    if (leading_comma == first) {
+      std::cerr << "error: unexpected format\n";
+      std::exit(1);
+    }
+    auto key_string = match[2];
+    auto value_string = match[3];
+    auto key = std::strtol(key_string.str().c_str(), nullptr, 10);
+    auto value = std::strtol(value_string.str().c_str(), nullptr, 10);
+    map.insert(std::make_pair(key, value));
+    first = false;
+    auto next = it;
+    ++next;
+    if (next == end) {
+      auto pos = it->position() + it->length();
+      if (pos != contents.length()) {
+        std::cerr << "error: unexpected format\n";
+        std::exit(1);
       }
     }
-    map.insert(std::make_pair(unzipped, i));
+    it = next;
   }
   return map;
 }
 
-static map_generator::IntMap readmap()
+IntMap read_map(const char *filename)
 {
-  map_generator::IntMap map{
-    {0, 0},
-    {1, 2},
-    {2, 3},
-    {100, 4},
-    {1897722, 5},
-  };
-  return map;
+  std::ifstream in(filename);
+  if (!in) {
+    std::cerr << "error: opening \"" << filename << "\"\n";
+    std::exit(1);
+  }
+  return read_map(in);
 }
 
-int main()
+static void usage()
 {
-  const unsigned max_stages = 1;
-  const auto map = testmap();
-  map_generator::Steps steps = map_generator::generate(map, max_stages);
+  std::cerr << "Usage: map_generator [options] [file]\n";
+  std::cerr << "\nOptions:\n";
+  std::cerr << "  --max-stages <n>    Use at most <n> lookups\n";
+}
+
+int main(int argc, char **argv)
+{
+  const char *filename = nullptr;
+  unsigned long max_stages = 1;
+  for (int i = 1; i < argc; ++i) {
+    std::string arg(argv[i]);
+    if (arg == "--max-stages") {
+      if (++i == argc) {
+        usage();
+        std::exit(1);
+      }
+      char *endp;
+      max_stages = strtoul(argv[i], &endp, 10);
+      if (*endp != '\0' || max_stages > std::numeric_limits<unsigned>::max()) {
+        usage();
+        std::exit(1);
+      }
+    } else if (arg == "--help") {
+      usage();
+      std::exit(0);
+    } else {
+      if (filename) {
+        usage();
+        std::exit(1);
+      }
+      filename = argv[i];
+    }
+  }
+  const auto map = filename ? read_map(filename) : read_map(std::cin);
+  Steps steps = map_generator::generate(map, max_stages);
   steps.print(std::cout);
-  map_generator::CodeCost cost = steps.compute_cost();
+  CodeCost cost = steps.compute_cost();
   std::cerr << "Number of bytes: " << cost.bytes << '\n';
   std::cerr << "Number of cycles: " << cost.cycles << '\n';
   return 0;
---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include "map_generator.h"
+#include <stdlib.h>
+#include <iostream>
+
+#define VERIFY(x) \
+  do { \
+    if (!(x)) { \
+      std::cerr << "FAILED: " << #x << '\n'; \
+      std::exit(1); \
+    } \
+  } while(0)
+
+// Map from module_zip of https://github.com/xcore/sc_advanced_port_usage
+static map_generator::IntMap unzip_map()
+{
+  map_generator::IntMap map;
+  for (unsigned i = 0; i < 256; i++) {
+    unsigned unzipped = 0;
+    for (unsigned j = 0; j < 8; j++) {
+      if (i & (1 << j)) {
+        unzipped |= 1 << (j * 4);
+      }
+    }
+    map.insert(std::make_pair(unzipped, i));
+  }
+  return map;
+}
+
+int main()
+{
+  map_generator::Steps steps = map_generator::generate(unzip_map(), 1);
+  map_generator::CodeCost cost = steps.compute_cost();
+  VERIFY(cost.bytes == 268);
+  return 0;
+}
